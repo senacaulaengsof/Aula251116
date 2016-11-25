7bbf08fd784c4ac3e6c7e5bed6529bc68a5fee96
senacaulaengsof <mrenginery@gmail.com>
2016-11-25 19:52:24 -0200

    Add files via upload

---
+++ medicamento.java
@@ -0,0 +1,50 @@
+package medicamento;
+
+public class medicamento {
+
+	private int id;
+	private int qtdeEstoque;
+	private String nome;
+	private String principioAtivo;
+	private String und;
+
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String novoNome){
+		this.nome = novoNome;
+}
+	public int getId(){
+		return id;
+	}
+	public void setId(int novaId){
+		this.id = novaId;
+	}
+	public int getQdteEstoque(){
+		return qtdeEstoque;
+	}
+	public void setQtdeEstoque(int novaqtdeEstoque){
+		this.qtdeEstoque = novaqtdeEstoque;
+	}
+	public String getPrincipioAtivo(){
+		return principioAtivo;
+	}
+	public void setPrincipioAtivo(String novoPrincipioAtivo){
+		this.principioAtivo = novoPrincipioAtivo;
+	}
+	public String getUnd(){
+		return und;
+	}
+	public void setUnd(String novaUnd){
+		this.und = novaUnd;
+	}
+	
+}
+	
+	
+	
+	
+	
+
+
---
+++ principalM.java
@@ -0,0 +1,25 @@
+package medicamento;
+
+public class principalM {
+
+	public static void main(String[] args) {
+		
+		medicamento novoMedicamento = new medicamento();
+		
+		novoMedicamento.setNome("Dramin");
+		novoMedicamento.setQtdeEstoque(100);
+		novoMedicamento.setUnd("1");
+		novoMedicamento.setId(20162015);
+		novoMedicamento.setPrincipioAtivo("Ativo");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
